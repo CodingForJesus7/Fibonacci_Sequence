fbf44cbf226d68a90407b4c81425f8967b42dfa7
CodingForJesus7 <parkerharris06@yahoo.com>
2026-08-04 01:19:13 -0500

    Add user input handling

--- fib_seq/src/main.rs
+++ fib_seq/src/main.rs
@@ -2,26 +2,19 @@
 //0 & 1 (not whichever two numbers you decide to start with).
 use std::io;
 fn main() {
-    println!("Input first number:");
-    let mut input1 = String::new();
+    println!(
+        "Input an nth (e.g. '3' (the third number in order of the Fibonacci Sequence will be 1)))"
+    );
+    let mut input = String::new();
     io::stdin()
-        .read_line(&mut input1)
-        .expect("Failed to read line");
-    let a: i32 = input1.trim().parse().expect("Input was NOT a number");
+        .read_line(&mut input)
+        .expect("Failed to read line.");
+    let input: i32 = input.trim().parse().expect("Error: Please type a number");
 
-    println!("Input second number:");
-    let mut input2 = String::new();
-    io::stdin()
-        .read_line(&mut input2)
-        .expect("Failed to read line");
-    let b: i32 = input2.trim().parse().expect("Input was NOT a number");
+    let mut counter = 0;
 
-    let mut counter = loop {
-        let mut c = a + b;
-
-        let a = b;
-
-        let c = b;
-    };
+    loop {
+        counter += 1;
+    }
 }
 //Grok Guide: https://grok.com/c/ee6c9144-bf9c-4888-a771-f610fc956cdf?rid=365371a2-4ac8-4740-a12d-f654ec432515
